1d0aa6f4210a3ac82121df810ddac3f7f6ad5f90
Munir Ali <thecodingsheykh63@gmail.com>
2023-04-24 17:37:21 +0300

    first-commit-react-app_with_tailwind-integration

---
+++ mule/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App flex justify-center">
+      <div className="layout  w-max bg-lime-700 mt-52">
+        <p className=" p-10 text-4xl ">Welcome, Main Layout here</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
